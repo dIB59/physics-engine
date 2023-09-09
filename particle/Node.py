b33a0453a6b3ab111def23f2a5b4cb4dc05743bb
Ibrahim <Ibrahim@ebs.net.pk>
2023-09-09 13:05:18 +0200

    implemented node for qtree

---
+++ particle/Node.py
@@ -0,0 +1,17 @@
+class Node:
+    def __init__(self, x0, y0, w, h, points):
+        self.x0 = x0
+        self.y0 = y0
+        self.width = w
+        self.height = h
+        self.points = points
+        self.children = []
+
+    def get_width(self):
+        return self.width
+
+    def get_height(self):
+        return self.height
+
+    def get_points(self):
+        return self.points
