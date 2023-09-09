7da886702bcc5552bc78a15114c24586c1bbb246
Ibrahim <Ibrahim@ebs.net.pk>
2023-09-09 13:08:41 +0200

    init commit

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+from particle.QTree import QTree
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    # Create a QuadTree with a threshold of 4 and 50 initial points.
+    qt = QTree(4, 100)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # Optionally, add more points.
+    qt.add_point(5, 5)
+
+    # Subdivide the QuadTree based on the threshold.
+    qt.subdivide()
+
+    # Visualize the QuadTree.
+    qt.graph()
