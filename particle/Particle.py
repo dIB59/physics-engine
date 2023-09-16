d1b0c462e9c13b65ca1daed37eb9c4e928e1dd7b
Ibrahim <Ibrahim@ebs.net.pk>
2023-09-16 23:23:22 +0200

    implemented particle class methods

--- particle/Particle.py
+++ particle/Particle.py
@@ -1,4 +1,25 @@
 class Particle:
-    def __init__(self, x, y):
+    def __init__(self, x: int, y: int, mass: int, vel_x: float, vel_y: float):
         self.x = x
         self.y = y
+        self.mass = mass
+        self.vel_x = vel_x
+        self.vel_y = vel_y
+
+    def update_position(self, dt: float):
+        self.x += self.vel_x * dt
+        self.y += self.vel_y * dt
+
+    def get_position(self) -> (int, int):
+        return self.x, self.y
+
+    def get_mass(self) -> int:
+        return self.mass
+
+    def get_velocity(self) -> (float, float):
+        return self.vel_x, self.vel_y
+
+    def set_velocity(self, vel_x: float, vel_y: float):
+        self.vel_x = vel_x
+        self.vel_y = vel_y
+
