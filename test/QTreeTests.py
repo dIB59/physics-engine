686912cf78f6559e3f1ecc4451ede9a1a0693d65
Ibrahim <Ibrahim@ebs.net.pk>
2023-09-16 23:17:27 +0200

    optimized commits

--- test/QTreeTests.py
+++ test/QTreeTests.py
@@ -7,25 +7,24 @@ project_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(project_dir)
 sys.path.append(parent_dir)
 
-from particle.QTree import QTree, _find_children
-
+import particle.QTree
 
 
 class QTreeTests(unittest.TestCase):
 
     def test_instantiation(self):
-        qt = QTree(4, 50)
+        qt = particle.QTree.QTree(4, 50)
         self.assertEqual(len(qt.get_points()), 50, f"Expected 50 points but got {len(qt.get_points())} points.")
 
     def test_add_point(self):
-        qt = QTree(4, 50)
+        qt = particle.QTree.QTree(4, 50)
         qt.add_point(5, 5)
         self.assertEqual(len(qt.get_points()), 51, f"Expected 51 points but got {len(qt.get_points())} points.")
 
     def test_subdivide(self):
-        qt = QTree(4, 50)
+        qt = particle.QTree.QTree(4, 50)
         qt.subdivide()
-        children = _find_children(qt.root)
+        children = particle.QTree._find_children(qt.root)
         self.assertGreaterEqual(len(children), 1, f"Expected at least 1 segment but got {len(children)}.")
 
 
