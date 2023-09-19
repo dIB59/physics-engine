a0f337a3fd15186022a731f9ac6f98034f619133
Ibrahim <Ibrahim@ebs.net.pk>
2023-09-19 16:15:29 +0200

    created particle class

---
+++ particle/Particle.py
@@ -0,0 +1,7 @@
+class Particle:
+    def __init__(self, x, y, mass, vel_x, vel_y):
+        self.x = x
+        self.y = y
+        self.mass = mass
+        self.vel_x = vel_x
+        self.vel_y = vel_y
