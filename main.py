1cbd3c1eb4904fc443e0717743bba5740ae2bde4
Ibrahim <Ibrahim@ebs.net.pk>
2023-09-23 18:08:56 +0200

    renamed graph method

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-import render.PygameRender
+from pygamelogic.PygameRender import render
 from util.QTree import QTree
 
 if __name__ == '__main__':
     # Create a QuadTree with a threshold of 4 and 50 initial points.
     qt = QTree(4, 25, 800, 800)
-    # Optionally, add more points.
-    qt.add_point(5, 5)
     # Subdivide the QuadTree based on the threshold.
     qt.subdivide()
     # Visualize the QuadTree.
-    qt.graph()
-    render.PygameRender.render(qt)
-    qt.graph()
+    qt.create_graph()
+    render(qt)
+    qt.create_graph()
 
 
